aeb5399ea74f3f44bd92c8a055ef8894e21de0ca
wjddns0122 <jwoon2004@gmail.com>
2025-11-19 22:19:51 +0900

    [학습] 별 모양 트리 구조 알고리즘 구현 완료

--- 9.JS/2.DOM/1.js
+++ 9.JS/2.DOM/1.js
@@ -4,12 +4,32 @@ for(let i = 1; i <= 5; i++) {
     console.log("*".repeat(i))
 }
 
+
+// 2번
 for(let i = 5; i >= 1; i --) {
     console.log("*".repeat(i))
 }
 
-// blank = "     "
-// tree = ""
-// for (let i = 0; i <= 5; i ++) {
-    
-// }
+// 3번
+function printTree3(height) {    
+    for (let i = 1; i <= height; i++) {
+        let space = " ".repeat(height - i);
+        let star = "*".repeat(i);
+        console.log(space + star);
+    }
+}
+
+let height = 5;
+printTree3(height)
+
+// 4번
+function printTree4(height) {    
+    for (let i = 0; i < height; i++) {
+        let space = " ".repeat(i);
+        let star = "*".repeat(height - i);
+        console.log(space + star);
+    }
+}
+
+// let height = 5;
+printTree4(height)
