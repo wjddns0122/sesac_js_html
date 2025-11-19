d23e4b8f29361a2966920d3436edad9c919cd6db
wjddns0122 <jwoon2004@gmail.com>
2025-11-19 16:05:55 +0900

    [학습] JS로 DOM을 제어하는것을 배움

---
+++ 9.JS/2.DOM/1.js
@@ -0,0 +1,15 @@
+
+// 1번
+for(let i = 1; i <= 5; i++) {
+    console.log("*".repeat(i))
+}
+
+for(let i = 5; i >= 1; i --) {
+    console.log("*".repeat(i))
+}
+
+// blank = "     "
+// tree = ""
+// for (let i = 0; i <= 5; i ++) {
+    
+// }
